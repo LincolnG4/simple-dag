b8bb550bd3127f5857f82df7885edf098eead8c1
LincolnG4 <g97santos@gmail.com>
2025-04-23 10:14:44 -0300

    dag created

--- dag.go
+++ dag.go
@@ -1,1 +1,138 @@
-package dag
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Node struct {
+	ID    string
+	Name  string
+	Edges []*Node
+	Task  func() error
+}
+
+func NewNode(name string, task func() error) *Node {
+	return &Node{
+		ID:   uuid.New().String(),
+		Name: name,
+		Task: task,
+	}
+}
+
+type Dag struct {
+	Nodes    map[string]*Node
+	inDegree map[string]int
+}
+
+func NewDag() *Dag {
+	return &Dag{
+		Nodes:    make(map[string]*Node),
+		inDegree: make(map[string]int),
+	}
+}
+
+func (d *Dag) AddNode(n *Node) error {
+	if _, ok := d.Nodes[n.ID]; ok {
+		return fmt.Errorf("node already exist")
+	}
+
+	d.Nodes[n.ID] = n
+	d.inDegree[n.ID] = 0
+	return nil
+}
+
+func (d *Dag) AddDependency(f, t *Node) error {
+	_, ok := d.Nodes[f.ID]
+	if !ok {
+		return fmt.Errorf("dependency source task '%s' not found", f.ID)
+	}
+	_, ok = d.Nodes[t.ID]
+	if !ok {
+		return fmt.Errorf("dependency target task '%s' not found", t.ID)
+	}
+
+	f.Edges = append(f.Edges, t)
+	d.inDegree[t.ID] += 1
+
+	return nil
+}
+
+func (d *Dag) Run() error {
+	queue := make([]*Node, 0)
+
+	for k, v := range d.inDegree {
+		if v == 0 {
+			queue = append(queue, d.Nodes[k])
+		}
+	}
+
+	for len(queue) > 0 {
+		v := queue[0]
+		//pop
+		queue = queue[1:]
+
+		err := v.Task()
+		if err != nil {
+			panic(err)
+		}
+
+		for _, edge := range v.Edges {
+			d.inDegree[edge.ID] -= 1
+
+			if d.inDegree[edge.ID] == 0 {
+				queue = append(queue, edge)
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func main() {
+
+	d := NewDag()
+
+	n1 := NewNode("task1", func() error {
+		fmt.Println("Doing 1")
+		time.Sleep(1 * time.Second)
+		return nil
+	})
+
+	err := d.AddNode(n1)
+	if err != nil {
+		panic(err)
+	}
+
+	n2 := NewNode("task2", func() error {
+		fmt.Println("Doing 2")
+		time.Sleep(1 * time.Second)
+		return nil
+	})
+
+	err = d.AddNode(n2)
+	if err != nil {
+		panic(err)
+	}
+
+	d.AddDependency(n1, n2)
+
+	n3 := NewNode("task3", func() error {
+		fmt.Println("Doing 3")
+		time.Sleep(1 * time.Second)
+		return nil
+	})
+
+	err = d.AddNode(n3)
+	if err != nil {
+		panic(err)
+	}
+
+	err = d.Run()
+	if err != nil {
+		panic(err)
+	}
+}
--- sample/go.mod
+++ sample/go.mod
@@ -1,3 +1,7 @@
 module sample-dag
 
 go 1.22.4
+
+require github.com/LincolnG4/go-dag v0.0.0-20250421133833-3ac083f33b83
+
+replace github.com/LincolnG4/go-dag => ../
